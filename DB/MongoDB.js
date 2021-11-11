aa2a1febadc2da6eb2b58f1d15cf9a5c18eda509
Mez <chhana7777@gmail.com>
2021-11-11 11:10:40 +0530

    goingback

--- DB/MongoDB.js
+++ DB/MongoDB.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async() => {
-    await mongoose.connect(`mongodb+srv://Mez:2X8QbCLat2qpc5vz@cluster0.6zaar.mongodb.net/chatapp?retryWrites=true&w=majority
-    `)
+    await mongoose.connect(process.env.MONGO_URI)
         .then(console.log('DB Connected'))
         .catch(err => console.log(err))
 }
